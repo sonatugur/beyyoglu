732f7c244f6fb8dff59e1a286604c57db2076fa5
sonat <sonugur@ku.edu.tr>
2016-03-23 23:15:53 +0200

    Added files via upload

---
+++ Magaza.swift
@@ -0,0 +1,19 @@
+//
+//  Magazalar.swift
+//  beyyogluDemo
+//
+//  Created by Sonat Ugur on 18/03/16.
+//  Copyright © 2016 Sonat Ugur. All rights reserved.
+//
+
+import Foundation
+
+class Magaza {
+    var magazaID: Int?
+    var magazaAdı: String?
+    var adres: String?
+    var urunler: [Magaza_Product]?
+    var xCoord: String?
+    var yCoord: String?
+    
+}
